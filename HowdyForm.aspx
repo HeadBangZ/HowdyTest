2ec6c81152198548750cb16b1de1a35289ca34c3
Thomas Nordvig Hermansen <thomasnhprivat@gmail.com>
2018-10-04 17:04:18 +0200

    first commit

---
+++ HowdyForm.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="HowdyForm.aspx.cs" Inherits="HowdyProject.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Howdy</title>
+	<link href="styles/styles.css" rel="stylesheet" />
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+			<h1 class="header-text">Welcome to Micro Howdy!</h1>
+			<div class="context-wrapper">
+				
+				<asp:Label ID="lb_taglineText" runat="server" Font-Bold="True" Font-Italic="True" Font-Size="X-Large" Text="Do you feel groovy?"></asp:Label>
+				<br />
+				<br />
+				<asp:Button ID="tryAgainBtn" CssClass="btn" runat="server" Text="Try Again?" OnClick="tryAgainBtn_Click" Visible="False" />
+				<br />
+				
+				<br />
+				<asp:Button ID="btnOne" CssClass="btn" runat="server" Text="Keep on dreamin'" OnClick="btnOne_Click" />
+				<br />
+				<br />
+				<asp:Button ID="btnTwo" CssClass="btn" runat="server" Text="You know it!" OnClick="btnTwo_Click" />
+				<br />
+				<br />
+				<asp:Button ID="btnThree" CssClass="btn" runat="server" Text="Off the hook" OnClick="btnThree_Click" />
+				<br />
+				<br />
+				<asp:Label ID="clickCounterValue" runat="server" Visible="False"></asp:Label>
+				<br />
+				<br />
+				<asp:Label ID="pointCount" runat="server" Visible="False">0</asp:Label>
+			</div>
+        </div>
+
+    </form>
+</body>
+</html>
---
+++ HowdyForm.aspx.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace HowdyProject
+{
+	public partial class WebForm1 : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		public void CallClickCountChecker(int point)
+		{
+			if (clickCounterValue.Text == "")
+			{
+				ChangeTextBtnAndLabel("Keep on livin'", "Riding the storm", "Feeling blue", "Dizzy miss Lizzy!", "0", point);
+			}
+			else if (clickCounterValue.Text == "1")
+			{
+				ChangeTextBtnAndLabel("How's it hangin", "Ace", "Primo", "Sublime", "1", point);
+			}
+			//else if (clickCounterValue.Text == "2")
+			//{
+			//	ChangeTextBtnAndLabel("Down with it?", "Superb", "Lovin' it", "The time of my life", "2", point);
+			//}
+			else
+			{
+				int currentPoint = int.Parse(pointCount.Text);
+				pointCount.Text = (currentPoint + point).ToString();
+
+				btnOne.Visible = false;
+				btnTwo.Visible = false;
+				btnThree.Visible = false;
+				tryAgainBtn.Visible = true;
+
+				switch (pointCount.Text)
+				{
+					case "3":
+						lb_taglineText.Text = "You are feeling way too blue";
+						break;
+					case "4":
+						lb_taglineText.Text = "Come on you are better than this";
+						break;
+					case "5":
+						lb_taglineText.Text = "You can't stay down here forever";
+						break;
+					case "6":
+						lb_taglineText.Text = "Wanna grab some food?";
+						break;
+					case "7":
+						lb_taglineText.Text = "Now we talking";
+						break;
+					case "8":
+						lb_taglineText.Text = "Yeehaa, this is great";
+						break;
+					case "9":
+						lb_taglineText.Text = "Wow are you leading the way to heaven";
+						break;
+				}
+			}
+		}
+
+		protected void btnOne_Click(object sender, EventArgs e)
+		{
+			CallClickCountChecker(1);
+		}
+
+		protected void btnTwo_Click(object sender, EventArgs e)
+		{
+			CallClickCountChecker(2);
+		}
+
+		protected void btnThree_Click(object sender, EventArgs e)
+		{
+			CallClickCountChecker(3);
+		}
+
+		public void ChangeTextBtnAndLabel(string lb_text, string btnTextOne, string btnTextTwo, string btnTextThree, string counter, int point)
+		{
+			int currentPoint = int.Parse(pointCount.Text);
+			pointCount.Text = (currentPoint + point).ToString();
+
+			int currentCount = int.Parse(counter);
+			currentCount++;
+			clickCounterValue.Text = currentCount.ToString();
+
+			lb_taglineText.Text = lb_text;
+			btnOne.Text = btnTextOne;
+			btnTwo.Text = btnTextTwo;
+			btnThree.Text = btnTextThree;
+		}
+
+		protected void tryAgainBtn_Click(object sender, EventArgs e)
+		{
+			pointCount.Text = "0";
+			clickCounterValue.Text = "";
+
+			lb_taglineText.Text = "Do you feel groovy?";
+			btnOne.Text = "Keep on dreamin'";
+			btnTwo.Text = "You know it!";
+			btnThree.Text = "Off the hook";
+
+			btnOne.Visible = true;
+			btnTwo.Visible = true;
+			btnThree.Visible = true;
+			tryAgainBtn.Visible = false;
+		}
+	}
+}
